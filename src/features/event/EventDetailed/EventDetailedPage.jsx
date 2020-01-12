768f6ccea84da75e8c52cf5a9d6acc04f262af78
Fred <frederic@pmo.digital>
2020-01-12 15:54:40 +0100

    etape 43 creation structure des dossiers et fichiers

---
+++ src/features/event/EventDetailed/EventDetailedPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const EventDetailedPage = () => {
+    return (
+        <div>
+            <h1>Détail de l'évènement</h1>
+        </div>
+    )
+}
+
+export default EventDetailedPage;
+

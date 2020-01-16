cdf755d230ec226905427a3037ceb24558c57e44
Frédéric BASTIAN <frederic@bastian.paris>
2020-01-16 09:03:42 +0100

    Etpae 54

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,13 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+
+const mapState = (state) => ({
+    data: state.data
+})
 
 class TestComponent extends Component {
     render() {
         return (
             <div>
                 <h1>Test Componant</h1>
+                <h3>The answer is: {this.props.data} </h3>
             </div>
         )
     }
 }
 
-export default TestComponent;
+export default connect(mapState)(TestComponent);

1164525caf2968535660934cc64c91fec45c09c6
Fred <frederic@pmo.digital>
2020-01-18 07:11:07 +0100

    Etape 58

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,8 +1,9 @@
 import { createStore } from "redux";
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import rootReducer from "../reducers/rootReducers";
 
 export const configureStore = () => {
-    const store = createStore(rootReducer)
+    const store = createStore(rootReducer, devToolsEnhancer())
 
     return store;
 }
